6daf9e63110777b057f6f941491d676361eda93d
na0260 <samjr.008@gmail.com>
2025-03-14 23:10:20 +0600

    autoload for classes added

---
+++ autoload.php
@@ -0,0 +1,9 @@
+<?php
+spl_autoload_register(function ($class) {
+    $class = str_replace("\\", DIRECTORY_SEPARATOR, $class);
+    $file = __DIR__ . '/classes/' . basename($class) . '.php';
+
+    if (file_exists($file)) {
+        require_once $file;
+    }
+});
