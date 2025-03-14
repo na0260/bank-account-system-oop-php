a6ea7aba3576877c2bf400b554530fb3a8ff1a67
na0260 <samjr.008@gmail.com>
2025-03-14 23:10:42 +0600

    overDraftLimit reduced

--- classes/CheckingAccount.php
+++ classes/CheckingAccount.php
@@ -6,14 +6,15 @@ use classes\Account;
 
 class CheckingAccount extends Account
 {
-    private $overdraftLimit = 1000;
+    private $overdraftLimit = 500;
     function withdraw($amount)
     {
         if ($amount > ($this->balance + $this->overdraftLimit)) {
             echo "Overdraft limit exceeded.".PHP_EOL;
-        } else{
-            $this->balance -= $amount;
-            echo "Withdrew $amount. New balance: $this->balance".PHP_EOL;
+            return;
         }
+
+        $this->balance -= $amount;
+        echo "Withdrew $amount. New balance: $this->balance".PHP_EOL;
     }
 }
